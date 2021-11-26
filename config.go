1f3bfd261d5ddcd5f5be45c7c9241b25ca2ba6d2
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-26 10:19:51 +0100

    Add config file

---
+++ config.go
@@ -0,0 +1,69 @@
+//********************************************************************************************************************//
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of gosendcloud.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package gosendcloud
+
+import (
+	"bytes"
+	"encoding/base64"
+	"net/http"
+)
+
+const (
+	transferProtocol = "https://"
+	baseUrl          = "panel.sendcloud.sc/api"
+	apiVersion       = "/v2"
+)
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	Body         []byte
+}
+
+// Request is to define the request data
+type Request struct {
+	publicKey string
+	secretKey string
+}
+
+// Send is to send a new request
+func (c Config) Send(r Request) (*http.Response, error) {
+
+	// Set url
+	url := transferProtocol + baseUrl + apiVersion + c.Path
+
+	// Create basic authentication
+	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(r.publicKey+":"+r.secretKey))
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Define header
+	request.Header.Set("Authorization", auth)
+	request.Header.Set("Content-Type", "application/json")
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
